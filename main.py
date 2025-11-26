812058416c7a8341c9204fbffe94c430df8a3ac6
liza_m <liza.mochalova.2014@mail.ru>
2025-11-27 00:17:21 +0300

    4

--- main.py
+++ main.py
@@ -18,15 +18,6 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 app.include_router(tasks_router)
 
-@app.get("/")
-def read_root():
-    return {
-        "message": "FastAPI Tasks API", 
-        "endpoints": {
-            "docs": "/docs",
-            "tasks": "/tasks",
-            "add_task": "POST /tasks"
-        }
-    }
+
 
 
